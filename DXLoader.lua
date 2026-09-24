a13ec63298f0e1a6a1413256cc191621743f3ef4
AomsinS4 <oppoppono93@gmail.com>
2026-09-24 06:58:51 +0000

    DXLoader.lua

--- DXLoader.lua
+++ DXLoader.lua
@@ -1,23 +1,23 @@
-local URL = "https://raw.githubusercontent.com/AomsinS4/DXPanel/main/DXClient.lua"
+local URL = "https://raw.githubusercontent.com/AomsinS4/DXPanel/refs/heads/main/DXClient.lua"
 
 local ok, source = pcall(function()
-	return game:HttpGet(URL)
+    return game:HttpGet(URL)
 end)
 
 if not ok then
-	warn("DXPanel download failed:", source)
-	return
+    warn("DXPanel download failed:", source)
+    return
 end
 
 local fn, err = loadstring(source)
 
 if not fn then
-	warn("DXPanel compile failed:", err)
-	return
+    warn("DXPanel compile failed:", err)
+    return
 end
 
 local success, result = pcall(fn)
 
 if not success then
-	warn("DXPanel execution failed:", result)
+    warn("DXPanel execution failed:", result)
 end
